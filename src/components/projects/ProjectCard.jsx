21d1e02367776a45e4b31b9c0932b74dce2fac43
David Märkisch <david.maerkisch@gmail.com>
2023-09-06 11:53:54 +0200

    layout experiences updated

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -7,7 +7,6 @@ const ProjectCard = ({ id, src, github, title, tech, desription }) => {
     <div
       key={id}
       className="shadow-lg shadow-gray-500 rounded-xl max-w-lg overflow-hidden cursor-pointer hover:scale-105 duration-500 lg:max-w-sm"
-      // hover:scale-105 duration-500
     >
       <a href={github} target="_blank">
         <img src={src} alt={title} className="drop-shadow-sm" />
