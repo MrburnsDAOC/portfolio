24236873aaa59131e3ec71ed08aa356a0b4c3e49
David Märkisch <david.maerkisch@gmail.com>
2023-11-02 13:57:23 +0100

    Updated projects: switched poker with portfolio, adjusted order

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import projectDci from "/project-dci.png";
-import projectPoker from "/project-poker.png";
+import projectPortfolio from "/project-portfolio.png";
 import projectAlpaca from "/project-alpaca.png";
 import ProjectCard from "./ProjectCard";
 
@@ -11,32 +11,32 @@ const Projects = () => {
   const projects = [
     {
       id: 1,
-      src: projectDci,
-      github: "https://github.com/MrburnsDAOC?tab=repositories",
-      title: "DCI final project",
+      src: projectPortfolio,
+      github: "https://github.com/MrburnsDAOC/portfolio",
+      title: "My portfolio page",
       desription:
-        "Currently working on my final DCI group project with three of my fellow colleagues. More information coming soon!",
-      tech: "React, CSS, Tailwind",
+        "This very portfolio you are on right now. Feel free to look into the code provided via the link!",
+      tech: "JS, React, Tailwind",
     },
 
     {
       id: 2,
-      src: projectPoker,
-      github: "https://github.com/MrburnsDAOC/carddeck",
-      title: "Poker logic",
-      desription:
-        "Some card logic like picking a random card from a deck of cards, shuffling the deck and giving a set number of players two cards each.",
-      tech: "JS",
-    },
-
-    {
-      id: 3,
       src: projectAlpaca,
       github: "https://github.com/MrburnsDAOC/alpaca",
       title: "Alpaca farm",
       desription:
         "Project to practice and learn CSS, working together in a small team on github as well as implementing of the design on basis the of a high-fidelity wireframe.",
       tech: "HTML, CSS, Sass",
+    },
+
+    {
+      id: 3,
+      src: projectDci,
+      github: "https://github.com/MrburnsDAOC?tab=repositories",
+      title: "DCI final project",
+      desription:
+        "Currently working on my final DCI group project with three of my fellow colleagues. More information coming soon!",
+      tech: "React, CSS, Tailwind",
     },
   ];
 
